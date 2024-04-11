d969de1c75860d9bf23a1ab59c138db137a25fb6
Litorian113 <154338974+Litorian113@users.noreply.github.com>
2024-04-12 01:07:17 +0200

    Setup Dataset Earthquakes
    Erste Visualisierung aller Daten auf der Map

--- Visualisierung/js/main.js
+++ Visualisierung/js/main.js
@@ -7,75 +7,60 @@ $(function () {
     stageHeight = renderer.innerHeight();
     stageWidth = renderer.innerWidth();
 
-    prepareData();
+    // prepareData();
     drawMap();
 });
 
-function prepareData() {
+// function prepareData() {
 
-    positionData.forEach(posCountry => {
-        populationData.forEach(popCountry => {
-            if (posCountry.alpha3Code === popCountry.alpha3Code) {
-                posCountry.population = popCountry.population;
-            }
-        });
-    });
-}
+//     positionData.forEach(posCountry => {
+//         populationData.forEach(popCountry => {
+//             if (posCountry.alpha3Code === popCountry.alpha3Code) {
+//                 posCountry.population = popCountry.population;
+//             }
+//         });
+//     });
+// }
 
 function drawMap() {
 
-    const populationMax = gmynd.dataMax(positionData, "population");
-    console.log("population max: " + populationMax);
+const magnitudeMax = gmynd.dataMax(earthquakeData, "Magnitude");
+console.log("magnitude max: " + magnitudeMax);
 
-    positionData.forEach(country => {
-        const area = gmynd.map(country.population, 0, populationMax, 5, 1000);
-        const r = gmynd.circleRadius(area)
+earthquakeData.forEach(earthquakeEvent => {
+    const area = gmynd.map(earthquakeEvent.Magnitude, 0, magnitudeMax, 5, 10);
+    const r = gmynd.circleRadius(area);
 
-        const x = gmynd.map(country.longitude, -180, 180, 0, stageWidth);
-        const y = gmynd.map(country.latitude, 90, -90, 0, stageHeight);
+    const x = gmynd.map(earthquakeEvent.Longitude, -180, 180, 0, stageWidth);
+    const y = gmynd.map(earthquakeEvent.Latitude, 90, -90, 0, stageHeight);
 
-        let dot = $('<div></div>');
-        dot.attr('country', country.country)
-
-        dot.mouseover( function (event) {
-            $(this).addClass('hover')
-            $('#hoverLabel').text(country.country)
-        })
-        dot.mouseout( function (event) {
-            $(this).removeClass('hover')
-            $('#hoverLabel').text(country.country)
-        })
-
-        dot.click(function (showLabel) {
-            $('.clickk').removeClass('clickk')
-            $(this).addClass('clickk')
-            $('#clickLabel').text(`${country.country}: ${country.population
-            }`)
-        })
-
-        let prozentAnteil = (country.population / populationMax) * 100
-        if(prozentAnteil <= 5) {
-            dot.css('background-color', 'green')
-        }else if(prozentAnteil > 5 && prozentAnteil <= 50) {
-            dot.css('background-color', 'yellow')
-        }else{
-            dot.css('background-color', 'red')
-        }
+    let dot = $('<div></div>');
+    dot.attr('earthquakeEvent', earthquakeEvent.Magnitude);
 
 
-        // mouseout
+    dot.mouseover( function (event) {
+        $(this).addClass('hover')
+        $('#hoverLabel').text(earthquakeEvent.Magnitude)
+    })
+    dot.mouseout( function (event) {
+        $(this).removeClass('hover')
+        $('#hoverLabel').text(earthquakeEvent.Magnitude)
+    })
 
+    dot.click(function (showLabel) {
+        $('.clickk').removeClass('clickk')
+        $(this).addClass('clickk')
+        $('#clickLabel').text(`${earthquakeEvent.Latitude}: ${earthquakeEvent.Longitude}`)
+    })
+    dot.addClass('dot')
 
-        // mouseclick
-
-        dot.addClass('dot')
-
-        dot.css({
-            'height': r,
-            'width': r,
-            'left': x,
-            'top': y
-        });
-        renderer.append(dot);
+    dot.css({
+        'height': r,
+        'width': r,
+        'left': x,
+        'top': y
     });
+    renderer.append(dot);
+
+});
 }
