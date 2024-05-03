18098e045708ca67a05f4501851eb5e9f56693fe
Litorian113 <154338974+Litorian113@users.noreply.github.com>
2024-05-03 16:53:47 +0200

    Sidemenu und Slider
    Große Anpassungen im CSS , Slider wurde überarbeitet , Filter eingebaut , Performance verbessert, Sidemenu mit Switch Buttons eingefügt.

--- Visualisierung/dataTsu.js
+++ Visualisierung/dataTsu.js
@@ -1,3 +1,4 @@
+
 let tsunamiData = [
     {
       "ID": 12,
--- Visualisierung/js/main.js
+++ Visualisierung/js/main.js
@@ -7,10 +7,159 @@ $(function () {
     stageHeight = renderer.innerHeight();
     stageWidth = renderer.innerWidth();
 
+    earthquakeData.forEach(function(item) {
+        // Datum in Tag, Monat und Jahr aufteilen
+        let dateParts = item["Date"].split(".");
+        // Hinzufügen von Tag, Monat und Jahr zum Datensatz
+        item["DAY"] = dateParts[0];
+        item["MONTH"] = dateParts[1];
+        let year = parseInt(dateParts[2]); // Konvertiere das Jahr in eine Zahl
+        // Füge je nach Bedingung das entsprechende Jahr hinzu
+        if (year <= 99 && year >= 65) {
+            year += 1900;
+        } else if (year >= 0 && year <= 16) {
+            year += 2000;
+        }
+        item["YEAR"] = year.toString(); // Konvertiere das Jahr zurück in einen String und speichere es im Datensatz
+        // Löschen des ursprünglichen Datums, falls erforderlich
+        // delete item["Date"];
+    });
+    // Sortiere den Datensatz nach dem Wert des Schlüssels "YEAR" aufsteigend
+tsunamiData.sort((a, b) => a.YEAR - b.YEAR);
+
+// Filtere die Einträge basierend auf dem Wert des Schlüssels "YEAR"
+tsunamiData = tsunamiData.filter(item => item.YEAR >= 1965 && item.YEAR <= 2016);
+
+
     // prepareData();
     drawEarthquakeMap();
     drawTsunamiMap();
 });
+
+$(function () {
+    let stageHeight = $('#renderer').innerHeight();
+    let stageWidth = $('#renderer').innerWidth();
+
+    // Initialanzeige der Erdbeben-Events für das Jahr 1965
+    // displayEarthquakes(1965);
+
+    // Globale Variablen für gefilterte Daten initialisieren
+let filteredEarthquakeData = [];
+let filteredTsunamiData = [];
+
+// Filterfunktion aufrufen, um Daten beim Laden der Seite zu filtern
+filterData();
+
+// Range Slider Event-Handler
+$('#yearRange').on('input', function () {
+    let selectedYear = $(this).val();
+    $('#currentYear').text(selectedYear);
+    // Funktion aufrufen, um Erdbeben- und Tsunami-Events für das ausgewählte Jahr anzuzeigen
+    displayEvents(selectedYear);
+});
+
+// Funktion zum Filtern der Daten
+function filterData() {
+    // Erdbeben-Daten filtern
+    filteredEarthquakeData = earthquakeData.filter(earthquakeEvent => {
+        let eventYear = parseInt(earthquakeEvent.YEAR);
+        return eventYear >= 1965 && eventYear <= 2016;
+    });
+
+    // Tsunami-Daten filtern
+    filteredTsunamiData = tsunamiData.filter(tsunamiEvent => {
+        let eventYear = parseInt(tsunamiEvent.YEAR);
+        return eventYear >= 1965 && eventYear <= 2016;
+    });
+}
+
+// Funktion zum Anzeigen der Erdbeben- und Tsunami-Events basierend auf dem ausgewählten Jahr
+function displayEvents(selectedYear) {
+    console.log("Selected Year:", selectedYear);
+    // Vorhandene Punkte entfernen, die nicht dem ausgewählten Jahr entsprechen
+    $('.dot, .dotTsu').remove();
+
+    // Erdbeben-Events für das ausgewählte Jahr anzeigen
+    filteredEarthquakeData.forEach(earthquakeEvent => {
+        if (earthquakeEvent.YEAR === selectedYear) {
+            // Anzeige des Erdbeben-Events
+            const area = gmynd.map(earthquakeEvent.Magnitude, 0, 10, 80, 80);
+            const r = gmynd.circleRadius(area);
+
+            const x = gmynd.map(earthquakeEvent.Longitude, -180, 180, 0, stageWidth);
+            const y = gmynd.map(earthquakeEvent.Latitude, 90, -90, 0, stageHeight);
+
+            let dot = $('<div></div>');
+            dot.attr('earthquakeEvent', earthquakeEvent.Magnitude);
+            let magnitudeLevel = earthquakeEvent.Magnitude;
+            if (magnitudeLevel <= 6) {
+                dot.css('background-color', 'yellow')
+            } else if (magnitudeLevel > 6 && magnitudeLevel <= 6.5) {
+                dot.css('background-color', 'orange')
+            } else {
+                dot.css('background-color', 'red')
+            }
+            dot.addClass('dot');
+            dot.css({
+                'height': r,
+                'width': r,
+                'left': x,
+                'top': y
+            });
+            $('#renderer').append(dot);
+        }
+    });
+
+    // Tsunami-Events für das ausgewählte Jahr anzeigen
+    filteredTsunamiData.forEach(tsunamiEvent => {
+        if (tsunamiEvent.YEAR == selectedYear) {
+            // Anzeige des Tsunami-Events
+            const intensityMax = gmynd.dataMax(filteredTsunamiData, "TS_INTENSITY");
+
+            const area = gmynd.map(tsunamiEvent.TS_INTENSITY, 0, intensityMax, 200, 200);
+            const r = gmynd.circleRadius(area);
+
+            const x = gmynd.map(tsunamiEvent.LONGITUDE, -180, 180, 0, stageWidth);
+            const y = gmynd.map(tsunamiEvent.LATITUDE, 90, -90, 0, stageHeight);
+
+            let dot = $('<div></div>');
+            dot.attr('tsunamiEvent', tsunamiEvent.TS_INTENSITY);
+            dot.addClass('dotTsu');
+            dot.css({
+                'height': r,
+                'width': r,
+                'left': x,
+                'top': y
+            });
+            $('#renderer').append(dot);
+        }
+    });
+    
+    }
+}); 
+//     tsunamiData.forEach(tsunamiEvent => {
+//         // Überprüfe, ob der Wert von "YEAR" größer oder gleich 1965 ist
+//         if (tsunamiEvent.YEAR >= 1965) {
+//             const area = gmynd.map(tsunamiEvent.TS_INTENSITY, 0, intensityMax, 200, 200);
+//             const r = gmynd.circleRadius(area);
+
+//             const x = gmynd.map(tsunamiEvent.LONGITUDE, -180, 180, 0, stageWidth);
+//             const y = gmynd.map(tsunamiEvent.LATITUDE, 90, -90, 0, stageHeight);
+
+//             let dot = $('<div></div>');
+//             dot.attr('tsunamiEvent', tsunamiEvent.TS_INTENSITY);
+//             dot.addClass('dotTsu');
+//             dot.css({
+//                 'height': r,
+//                 'width': r,
+//                 'left': x,
+//                 'top': y
+//             });
+//             renderer.append(dot);
+//         }
+//     });
+// }
+
 
 // function prepareData() {
 
@@ -23,7 +172,7 @@ $(function () {
 //     });
 // }
 function drawTsunamiMap() {
-    const intensityMax = gmynd.dataMax(tsunamiData, "TS_INTENSITY");
+    let intensityMax = gmynd.dataMax(tsunamiData, "TS_INTENSITY");
     console.log("intensity max: " + intensityMax);
 
     tsunamiData.forEach(tsunamiEvent => {
@@ -152,3 +301,10 @@ $('.dot').mouseover(function(event) {
 });
 
 }
+
+$(document).ready(function() {
+    $('#togglePanel').click(function() {
+        $('.controlPanel').toggleClass('open'); /* Fügt oder entfernt die Klasse 'open' */
+        $('#togglePanel').toggleClass('open'); /* Fügt oder entfernt die Klasse 'open' */
+    });
+});
